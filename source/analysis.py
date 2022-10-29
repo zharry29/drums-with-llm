e64547ef5011b8d9011ef884ae8af736ea9f38ee
Harry Zhang <zharry@umich.edu>
2022-10-28 23:54:06 -0400

    Ada

--- source/analysis.py
+++ source/analysis.py
@@ -29,25 +29,26 @@ def repetition_variation(lines):
     stabilities = [min([levenshtein_distance(measures[i],measures[i-1]),levenshtein_distance(measures[i],measures[i+1])]) for i in range(1, len(measures)-1)]
     return stabilities
 
-def plot_stabilities(gold_stabilities, davinci_stabilities, curie_stabilities, random_stabilities, repeat_stabilities, fname):
+def plot_stabilities(gold_stabilities, davinci_stabilities, ada_stabilities, random_stabilities, repeat_stabilities, fname):
     plt.plot(range(2,len(gold_stabilities)+2), gold_stabilities, '-o', label='human')
     plt.plot(range(2,len(random_stabilities)+2), random_stabilities, '-1', label='random')
     plt.plot(range(2,len(repeat_stabilities)+2), repeat_stabilities, '-2', label='repeat')
     plt.plot(range(2,len(davinci_stabilities)+2), davinci_stabilities, '-v', label='davinci')
-    plt.plot(range(2,len(curie_stabilities)+2), curie_stabilities, '-^', label='curie')
+    plt.plot(range(2,len(davinci_stabilities)+2), davinci_stabilities, '-^', label='ada')
     plt.title(fname)
     plt.xlabel("Measure")
     plt.ylabel("Stability")
-    #plt.legend(loc='best')
+    plt.legend(loc='best')
     plt.ylim([0, 50])
     #plt.show()
-    plt.savefig(f"../output_figs/{fname}.png")
+    plt.savefig(f"../output_figs/legend.png")
+    raise SystemExit()
     plt.clf()
 
 def average_distance_to_cluster_center(stabilities):
     stabilities = np.array(stabilities)
     kmeans = KMeans(n_clusters=2, random_state=0).fit(stabilities.reshape(-1, 1))
-    return np.mean(np.abs(stabilities - kmeans.cluster_centers_[kmeans.labels_][:,0]))
+    return np.abs(kmeans.cluster_centers_[:,0][0]-kmeans.cluster_centers_[:,0][1]),np.mean(np.abs(stabilities - kmeans.cluster_centers_[kmeans.labels_][:,0]))
 
 
 fnames = os.listdir("../data_text/dev")
@@ -55,39 +56,38 @@ gold_all_stabilities = []
 random_all_stabilities = []
 repeat_all_stabilities = []
 davinci_all_stabilities = []
-curie_all_stabilities = []
+ada_all_stabilities = []
 for fname in fnames:
     fname = fname.split('.')[0]
-    with open(f"../data_text/dev/{fname}.txt") as fg, open(f"../output_text/dev/{fname}.txt") as fd, open(f"../output_text_curie/dev/{fname}.txt") as fc:
+    with open(f"../data_text/dev/{fname}.txt") as fg, open(f"../output_text/dev/{fname}.txt") as fd, open(f"../output_text_ada/dev/{fname}.txt") as fc:
         gold_lines = fg.readlines()
         davinci_lines = fd.readlines()
-        curie_lines = fc.readlines()
+        ada_lines = fc.readlines()
 
     gold_stabilities = repetition_variation(gold_lines)
     davinci_stabilities = repetition_variation(davinci_lines)
-    curie_stabilities = repetition_variation(curie_lines)
+    ada_stabilities = repetition_variation(ada_lines)
     random_stabilities = repetition_variation([''.join([random.choice(['-','o']) for j in range(6)]) + '\n' for i in range(256)])
     repeat_stabilities = repetition_variation(gold_lines[:16] + gold_lines[16:32]*15)
     
-    gold_all_stabilities.append(average_distance_to_cluster_center(gold_stabilities))
-    random_all_stabilities.append(average_distance_to_cluster_center(random_stabilities))
+    #gold_all_stabilities.append(average_distance_to_cluster_center(gold_stabilities)[1])
+    #random_all_stabilities.append(average_distance_to_cluster_center(random_stabilities)[1])
     #repeat_all_stabilities += average_distance_to_cluster_center(repeat_stabilities)
-    davinci_all_stabilities.append(average_distance_to_cluster_center(davinci_stabilities))
-    curie_all_stabilities.append(average_distance_to_cluster_center(curie_stabilities))
-
+    #davinci_all_stabilities.append(average_distance_to_cluster_center(davinci_stabilities)[1])
+    #ada_all_stabilities.append(average_distance_to_cluster_center(ada_stabilities)[0])
+    
     #gold_all_stabilities += gold_stabilities
     #random_all_stabilities += random_stabilities
     #repeat_all_stabilities +=repeat_stabilities
     #davinci_all_stabilities += davinci_stabilities
-    #curie_all_stabilities += curie_stabilities
+    #ada_all_stabilities += ada_stabilities
 
-    #plot_stabilities(gold_stabilities, davinci_stabilities, curie_stabilities, random_stabilities, repeat_stabilities, fname)
+    plot_stabilities(gold_stabilities, davinci_stabilities, ada_stabilities, random_stabilities, repeat_stabilities, fname)
     #break
 
 
-
-print(mean(gold_all_stabilities))
-print(mean(random_all_stabilities))
+#print(mean(gold_all_stabilities))
+#print(mean(random_all_stabilities))
 #print(mean(repeat_all_stabilities))
-print(mean(davinci_all_stabilities))
-print(mean(curie_all_stabilities))
+#print(mean(davinci_all_stabilities))
+#print(mean(ada_all_stabilities))
--- source/evaluate.py
+++ source/evaluate.py
@@ -1,12 +1,19 @@
 import json
 import openai
 from text_to_midi import convert_to_midi
+import sys
 
 openai.api_key_path = "../source/api_keys/harry.key"
+model = sys.argv[1]
+model_to_name = {
+    "davinci": "davinci:ft-ccb-lab-members-2022-10-18-23-45-36",
+    "curie": "curie:ft-ccb-lab-members-2022-10-28-02-14-43",
+    "ada": "ada:ft-ccb-lab-members-2022-10-29-02-43-53"
+}
 
 def prompt_gpt3(prompt):
     res = openai.Completion.create(
-        model="davinci:ft-ccb-lab-members-2022-10-18-23-45-36",
+        model=model_to_name[model],
         prompt=prompt,
         temperature=0.9,
         max_tokens=1000,
@@ -25,10 +32,10 @@ for split in ["dev", "test"]:
             #print(prompt)
             #print('========')
             #print(gen)
-            with open(f"../output_text/{split}/{name}.txt", 'w') as fw:
+            with open(f"../output_text_{model}/{split}/{name}.txt", 'w') as fw:
                 fw.write(gen_score)
             try:
-                convert_to_midi(gen_score, f"../output_midi/{split}/{name}.mid")
+                convert_to_midi(gen_score, f"../output_midi_{model}/{split}/{name}.mid")
             except:
                 print(name)
                 print("Error")
