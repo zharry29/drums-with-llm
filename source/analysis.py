030ce232c29c87613f3fbdc4c029b04eafa57c17
Harry Zhang <zharry@umich.edu>
2022-10-30 22:07:22 -0400

    Redo quantitative analysis

--- source/analysis.py
+++ source/analysis.py
@@ -5,6 +5,9 @@ random.seed(29)
 from statistics import mean
 from sklearn.cluster import KMeans
 import numpy as np
+import sys
+
+metric = sys.argv[1] # plot, mean, inter, intra
 
 def levenshtein_distance(s1, s2):
     if len(s1) > len(s2):
@@ -34,15 +37,16 @@ def plot_stabilities(gold_stabilities, davinci_stabilities, ada_stabilities, ran
     plt.plot(range(2,len(random_stabilities)+2), random_stabilities, '-1', label='random')
     plt.plot(range(2,len(repeat_stabilities)+2), repeat_stabilities, '-2', label='repeat')
     plt.plot(range(2,len(davinci_stabilities)+2), davinci_stabilities, '-v', label='davinci')
-    plt.plot(range(2,len(davinci_stabilities)+2), davinci_stabilities, '-^', label='ada')
+    plt.plot(range(2,len(ada_stabilities)+2), ada_stabilities, '-^', label='ada')
     plt.title(fname)
-    plt.xlabel("Measure")
-    plt.ylabel("Stability")
-    plt.legend(loc='best')
+    #plt.xlabel("Measure")
+    #plt.ylabel("Stability")
+    #plt.legend(loc='best')
     plt.ylim([0, 50])
     #plt.show()
-    plt.savefig(f"../output_figs/legend.png")
-    raise SystemExit()
+    #plt.savefig(f"../output_figs/legend.png")
+    plt.savefig(f"../output_figs/{fname}.png")
+    #raise SystemExit()
     plt.clf()
 
 def average_distance_to_cluster_center(stabilities):
@@ -51,7 +55,7 @@ def average_distance_to_cluster_center(stabilities):
     return np.abs(kmeans.cluster_centers_[:,0][0]-kmeans.cluster_centers_[:,0][1]),np.mean(np.abs(stabilities - kmeans.cluster_centers_[kmeans.labels_][:,0]))
 
 
-fnames = os.listdir("../data_text/dev")
+fnames = os.listdir("../data_text/test")
 gold_all_stabilities = []
 random_all_stabilities = []
 repeat_all_stabilities = []
@@ -59,7 +63,7 @@ davinci_all_stabilities = []
 ada_all_stabilities = []
 for fname in fnames:
     fname = fname.split('.')[0]
-    with open(f"../data_text/dev/{fname}.txt") as fg, open(f"../output_text/dev/{fname}.txt") as fd, open(f"../output_text_ada/dev/{fname}.txt") as fc:
+    with open(f"../data_text/test/{fname}.txt") as fg, open(f"../output_text_davinci/test/{fname}.txt") as fd, open(f"../output_text_ada/test/{fname}.txt") as fc:
         gold_lines = fg.readlines()
         davinci_lines = fd.readlines()
         ada_lines = fc.readlines()
@@ -70,24 +74,30 @@ for fname in fnames:
     random_stabilities = repetition_variation([''.join([random.choice(['-','o']) for j in range(6)]) + '\n' for i in range(256)])
     repeat_stabilities = repetition_variation(gold_lines[:16] + gold_lines[16:32]*15)
     
-    #gold_all_stabilities.append(average_distance_to_cluster_center(gold_stabilities)[1])
-    #random_all_stabilities.append(average_distance_to_cluster_center(random_stabilities)[1])
-    #repeat_all_stabilities += average_distance_to_cluster_center(repeat_stabilities)
-    #davinci_all_stabilities.append(average_distance_to_cluster_center(davinci_stabilities)[1])
-    #ada_all_stabilities.append(average_distance_to_cluster_center(ada_stabilities)[0])
+    if metric != 'plot' and metric != 'mean':
+        if metric == 'intra':
+            i = 0 
+        elif metric == 'inter':
+            i = 1
+        gold_all_stabilities.append(average_distance_to_cluster_center(gold_stabilities)[i])
+        random_all_stabilities.append(average_distance_to_cluster_center(random_stabilities)[i])
+        repeat_all_stabilities.append(average_distance_to_cluster_center(repeat_stabilities)[i])
+        davinci_all_stabilities.append(average_distance_to_cluster_center(davinci_stabilities)[i])
+        ada_all_stabilities.append(average_distance_to_cluster_center(ada_stabilities)[i])
     
-    #gold_all_stabilities += gold_stabilities
-    #random_all_stabilities += random_stabilities
-    #repeat_all_stabilities +=repeat_stabilities
-    #davinci_all_stabilities += davinci_stabilities
-    #ada_all_stabilities += ada_stabilities
+    elif metric == 'mean':
+        gold_all_stabilities += gold_stabilities
+        random_all_stabilities += random_stabilities
+        repeat_all_stabilities +=repeat_stabilities
+        davinci_all_stabilities += davinci_stabilities
+        ada_all_stabilities += ada_stabilities
 
-    plot_stabilities(gold_stabilities, davinci_stabilities, ada_stabilities, random_stabilities, repeat_stabilities, fname)
-    #break
+    else: #plot
+        plot_stabilities(gold_stabilities, davinci_stabilities, ada_stabilities, random_stabilities, repeat_stabilities, fname)
 
-
-#print(mean(gold_all_stabilities))
-#print(mean(random_all_stabilities))
-#print(mean(repeat_all_stabilities))
-#print(mean(davinci_all_stabilities))
-#print(mean(ada_all_stabilities))
+if metric != 'plot':
+    print(mean(gold_all_stabilities))
+    print(mean(random_all_stabilities))
+    print(mean(repeat_all_stabilities))
+    print(mean(davinci_all_stabilities))
+    print(mean(ada_all_stabilities))
